943d3b452ec785988ca10f7a3f24e2b599c25991
AhmedKhatri510 <ahmedkhatri510@gmail.com>
2021-09-12 21:44:48 +0530

    getting position of user and fetching the weather condition based on the location

---
+++ script.js
@@ -0,0 +1,42 @@
+//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+const API_KEY = "d0f8a8fc77158dd6c4aa3d46d038afea";
+const URL = `http://api.openweathermap.org/data/2.5/weather?`;
+const KELVIN = 273;
+
+const iconEl = document.querySelector(".details img");
+const temperatureEl = document.querySelector(".temperature");
+const descriptionEl = document.querySelector(".description");
+const citycountryEl = document.querySelector(".current-city-country");
+
+const getWeatherData = async function (lat, lon) {
+  const response = await fetch(`${URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+  const data = await response.json();
+
+  console.log(data);
+
+  let { temp } = data.main;
+  temp = Math.floor(temp - KELVIN);
+  const { description, icon } = data.weather[0];
+  const { name } = data;
+  const { country } = data.sys;
+
+  iconEl.setAttribute("src", `./icons/${icon}.png`);
+  // temperatureEl.textContent = `${temp}`;
+  temperatureEl.childNodes[0].data = `${temp}° `;
+  descriptionEl.textContent = description;
+  citycountryEl.textContent = `${name}, ${country}`;
+
+  console.log(temp, description, icon, name, country);
+};
+
+const getPosition = function (position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  getWeatherData(lat, lon);
+};
+
+navigator.geolocation.getCurrentPosition(
+  getPosition,
+  console.log("location not found")
+);
