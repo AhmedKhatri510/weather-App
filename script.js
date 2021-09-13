0f1db39a3727d4b57f5456074b9ce2e323b300c6
AhmedKhatri510 <ahmedkhatri510@gmail.com>
2021-09-13 12:28:29 +0530

    toggle celsius to fahrenhite and vice versa

--- script.js
+++ script.js
@@ -2,12 +2,37 @@
 const API_KEY = "d0f8a8fc77158dd6c4aa3d46d038afea";
 const URL = `https://api.openweathermap.org/data/2.5/weather?`;
 const KELVIN = 273;
+let tempUnit = "celsius";
 
 const iconEl = document.querySelector(".details img");
 const temperatureEl = document.querySelector(".temperature");
 const descriptionEl = document.querySelector(".description");
 const citycountryEl = document.querySelector(".current-city-country");
 const notificationEl = document.querySelector(".notification");
+
+const changeTempUnit = function () {
+  if (tempUnit === "celsius") {
+    tempUnit = "fahrenhite";
+
+    const tempInfahrenhite = Math.round(
+      (Number(temperatureEl.childNodes[0].data.split(" ")[0]) * 9) / 5 + 32
+    );
+
+    temperatureEl.childNodes[0].textContent = `${tempInfahrenhite} ° `;
+    temperatureEl.children[0].textContent = "F";
+  } else if (tempUnit === "fahrenhite") {
+    tempUnit = "celsius";
+    // temperatureEl.childNodes[0].data = `${temp}° `;
+    // (32°F − 32) × 5/9 = 0°C
+    const tempIncelsius = Math.round(
+      (+temperatureEl.childNodes[0].data.split(" ")[0] - 32) * (5 / 9)
+    );
+    temperatureEl.childNodes[0].textContent = `${tempIncelsius} ° `;
+    temperatureEl.children[0].textContent = "C";
+  }
+};
+
+temperatureEl.addEventListener("click", changeTempUnit);
 
 const getWeatherData = async function (lat, lon) {
   const response = await fetch(`${URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`);
@@ -23,7 +48,7 @@ const getWeatherData = async function (lat, lon) {
 
   iconEl.setAttribute("src", `./icons/${icon}.png`);
   // temperatureEl.textContent = `${temp}`;
-  temperatureEl.childNodes[0].data = `${temp}° `;
+  temperatureEl.childNodes[0].data = `${temp} ° `;
   descriptionEl.textContent = description;
   citycountryEl.textContent = `${name}, ${country}`;
 
